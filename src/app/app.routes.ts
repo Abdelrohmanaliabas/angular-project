f3f59763534b4a5f7b746f7c9ce91f7bf0592bb1
AbdullahShokr1 <darkthe254@gmail.com>
2025-10-13 17:29:36 +0300

    Auth dark mode

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Auth } from './auth/auth';
 import { Register } from './auth/register';
+import { Dashboard } from './components/dashboard/dashboard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: Auth },
   { path: 'register', component: Register },
+  { path: 'dashboard', component: Dashboard },
 ];
