66df3a00b9655e8e96ec7c46541f8b810d7e17e3
Alaa Amr <alaaamr153@gmail.com>
2025-10-16 03:28:39 +0200

    dashboards components

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,12 +1,24 @@
+import { Locations } from './locations/locations';
+// import { EventList } from './events/event-list/event-list';
 import { Component } from '@angular/core';
 import { Sidebar } from '../shared/sidebar/sidebar';
 import { Navbar } from '../shared/navbar/navbar';
-import { EventCreation } from './events/event-creation/event-creation';
-
+// import { TabViewModule } from 'primeng/tabview';
+// import { EventCreation } from './events/event-creation/event-creation';
+// import { Main } from './main/main';
+// import { SpeakerList } from './speaker-list/speaker-list';
+// import { Profile } from './profile/profile';
+// import { Calender } from './calender/calender';
+// import { Schedule } from './schedule/schedule';
+// import { AttendList } from './attend-list/attend-list';
+// import { EventDetails } from './events/event-details/event-details';
+// import { ProfileSetting } from './profile/profile-setting/profile-setting';
+// import { AttendList } from './attend-list/attend-list';
+// import { EventList } from './events/event-list/event-list';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Sidebar ,Navbar ,EventCreation],
+  imports: [Sidebar,Navbar,Locations],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
